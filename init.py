1e7976ea80b6cd5dfcb6ee203334065475e53544
sai109 <lukebraithwaite7@gmail.com>
2018-02-25 10:42:41 +0000

    Assets of man added

--- init.py
+++ init.py
@@ -1,8 +1,14 @@
 import pygame
-pygame.init()
-
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
-
-screen = pygame.display.set_mode(size)
+import glob
+# import pyganim
+#
+# pygame.init()
+#
+# size = width, height = 320, 240
+# speed = [2, 2]
+# black = 0, 0, 0
+#
+# screen = pygame.display.set_mode(size)
+array = glob.glob('*.png')
+finalArray = list(map(lambda x: (x, 200), array))
+print(finalArray)
